6d9d252bad7646d584513c57e88e23329967e71f
Ld-monkey <lfourteau91@gmail.com>
2024-04-22 12:38:32 +0200

    finish the global design

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,46 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Avatar from '../../assets/images/avatar-jessica.jpeg';
+
 function App() {
+  const socialNetworks = [
+    'Github',
+    'Frontend Mentor',
+    'Linkedin',
+    'Twitter',
+    'Instagram',
+  ];
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Hello World</h1>
-    </div>
+    <main className="flex h-screen items-center justify-center bg-black text-white">
+      <div className="bg-dark-gray flex min-w-[327px] flex-col items-center rounded-xl p-8">
+        <img
+          src={Avatar}
+          alt="Avatar profile"
+          className="h-[88px] w-[88px] rounded-full"
+        />
+        <h1 className="mt-4 text-2xl">Jessica Randall</h1>
+        <h2 className="text-neon-green mt-1 text-sm font-bold">
+          London, United KIngdom
+        </h2>
+        <div className="mt-8">
+          <p className="text-sm">
+            &quot;Front-end developer and avid reader.&quot;
+          </p>
+        </div>
+        <ul className="mt-6 w-full space-y-4 font-bold">
+          {socialNetworks.map((item) => (
+            <li key={item}>
+              <a
+                href="#"
+                className="bg-grey block h-full w-full rounded-lg p-3 text-center hover:underline"
+              >
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#141414',
+        'dark-gray': '#1F1F1F',
+        'neon-green': '#C4F82A',
+        grey: '#333333',
+      },
+    },
   },
   plugins: [],
-}
-
+};
