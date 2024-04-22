54967b02e4bb9560f6cb98c89091394212a61b9b
Ld-monkey <lfourteau91@gmail.com>
2024-04-22 14:49:09 +0200

    finish small social project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@
 import Avatar from '../../assets/images/avatar-jessica.jpeg';
 
 function App() {
-  const socialNetworks = [
+  const socialLinks = [
     'Github',
     'Frontend Mentor',
     'Linkedin',
@@ -11,30 +11,32 @@ function App() {
   ];
 
   return (
-    <main className="flex h-screen items-center justify-center bg-black text-white">
-      <div className="bg-dark-gray flex min-w-[327px] flex-col items-center rounded-xl p-8">
-        <img
-          src={Avatar}
-          alt="Avatar profile"
-          className="h-[88px] w-[88px] rounded-full"
-        />
-        <h1 className="mt-4 text-2xl">Jessica Randall</h1>
-        <h2 className="text-neon-green mt-1 text-sm font-bold">
-          London, United KIngdom
-        </h2>
-        <div className="mt-8">
+    <main className="flex h-screen flex-col items-center justify-center bg-black text-white">
+      <div className="bg-dark-gray container flex w-10/12 max-w-[384px] flex-col items-center rounded-xl p-6 sm:p-10">
+        <div className="flex flex-col items-center gap-6">
+          <img
+            src={Avatar}
+            alt="Avatar profile"
+            className="size-[88px] rounded-full"
+          />
+          <div className="space-y-1 text-center">
+            <h1 className="text-2xl font-semibold">Jessica Randall</h1>
+            <h2 className="text-neon-green text-sm font-bold">
+              London, United KIngdom
+            </h2>
+          </div>
           <p className="text-sm">
             &quot;Front-end developer and avid reader.&quot;
           </p>
         </div>
         <ul className="mt-6 w-full space-y-4 font-bold">
-          {socialNetworks.map((item) => (
-            <li key={item}>
+          {socialLinks.map((link) => (
+            <li key={link}>
               <a
                 href="#"
                 className="bg-gray hover:bg-neon-green hover:text-gray block h-full w-full rounded-lg p-3 text-center transition-colors delay-75 duration-200 ease-out"
               >
-                {item}
+                {link}
               </a>
             </li>
           ))}
